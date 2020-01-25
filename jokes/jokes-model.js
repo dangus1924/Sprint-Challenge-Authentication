5fd96601983f7d88cbcaff6496fb2483ab8467b4
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-25 12:54:58 -0500

    MVP completed for dad jokes model

---
+++ jokes/jokes-model.js
@@ -0,0 +1,31 @@
+const db = require('../database/dbConfig')
+
+function find() {
+    return db('users')
+}
+
+function findBy(filter) {
+    return db('users').where(filter)
+}
+
+function findById(id) {
+    return db('users').where({ id }).first()
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user)
+    return findById(id)
+}
+
+function remove(id) {
+    return db('users').where({ id }).del()
+}
+
+
+module.exports = {
+    find,
+    findBy,
+    findById,
+    add,
+    remove
+}
